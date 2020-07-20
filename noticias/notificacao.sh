8914eb8ca5c9d798bcfdf6def22a89072a45f889
bernecg <bernard.caye@gmail.com>
2020-07-20 15:35:51 -0300

    Slack hook url passed by env var

--- noticias/notificacao.sh
+++ noticias/notificacao.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
-curl -X POST --data-urlencode \
-"payload={
-    \"channel\":\"#proj-alura-noticias\",
-    \"username\": \"webhookbot\",
-    \"text\": \"New News app was initialized.\",
-    \"icon_emoji\": \":ghost:\"
-}" \
-https://hooks.slack.com/services/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
+if [ -z $SLACK_URL ];then
+    echo "[WARN] SLACK_URL not setted, no notifications of new app sent..."
+else
+    curl -X POST --data-urlencode \
+    "payload={
+        \"channel\":\"#proj-alura-noticias\",
+        \"username\": \"webhookbot\",
+        \"text\": \"New News app was initialized.\",
+        \"icon_emoji\": \":ghost:\"
+    }" \
+    $SLACK_URL
+fi
